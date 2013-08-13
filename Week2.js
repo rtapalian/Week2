eb9e5636c3b0c4aca1e28fa8733df04d5543e3cc
rtapalian <rtapalian@email.neit.edu>
2013-08-13 17:22:17 -0400

    Update Week2.js

--- Week2.js
+++ Week2.js
@@ -1,8 +1,11 @@
-  /*
+ /*
 Author: Richard Tapalian
 Week 2
 Date: 08/07/2013
 */
+/*Main Function activated by submit button
+all variables are defined below.  Information is taken from
+the form that is filled out by the user.*/
 function submitForm() {
 var name = document.getElementById('int-name');
 var err_name = document.getElementById('err_name');
@@ -16,10 +19,13 @@ var myform=document.getElementById("myform");
 var confirmation=document.getElementById("confirmation");
 var confirm_results=document.getElementById("confirm_results");
 
-var hasErrors = false;
 
+
+
+var hasErrors = false;
+/*Checks to see if there is a first name entered*/
 if (!name.value.length) {
-err_name.innerHTML = "*";/*"<em>Please enter a first name</em>";*/
+err_name.innerHTML = "*";
 name.className = "bad";
 hasErrors = true;
 }
@@ -30,9 +36,9 @@ name.className = "good";
 
 
 
-
+/*Checks to see if there is a last name entered*/
 if (!lname.value.length) {
-err_lname.innerHTML = "*";/*"<em>Please enter your last name</em>";*/
+err_lname.innerHTML = "*";
 lname.className = "bad";
 hasErrors = true;
 }
@@ -43,10 +49,10 @@ lname.className = "good";
 
 
 
-
+/*Checks to make sure that email is properly formated with a period and @ symbol*/
 if ( !email.value.length )
 {
-err_email.innerHTML = "*";/*"<em>Please Fill out your email</em>";*/
+err_email.innerHTML = "*";
 email.className = "bad";
 hasErrors = true;
 } else if (email.value.indexOf("@") == -1)
@@ -67,7 +73,7 @@ email.className = "good";
 }
 
 
-
+/*Checks to see if the comment length is between 0 and 150 characters*/
 if (comments.value.length==0)
 {
 err_comments.innerHTML = "*";
@@ -84,7 +90,7 @@ else {
 err_comments.innerHTML = "";
 comments.className = "good";
 }
-
+/*Takes the good data entered and displays a confirmation page*/
 var stringconfirm = "";
 if (!hasErrors){
 	myform.style.display="none";
